3b0e3e16072a1a545ac1c772691d293b0e14f811
Noah Lim <91938285+noahwhlim@users.noreply.github.com>
2024-09-10 22:29:21 -0500

    Add files via upload

--- 191-number-of-1-bits.py
+++ 191-number-of-1-bits.py
@@ -6,3 +6,21 @@ class Solution:
             if c == '1':
                 count += 1
         return count
+        
+# neetcode solution #1
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         res = 0
+#         while n:
+#             res += n % 2
+#             n = n >> 1
+#         return res
+
+# neetcode solution #2
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         res = 0
+#         while n:
+#             n &= n - 1    # basically gets rid of next 1
+#             res += 1
+#         return res
